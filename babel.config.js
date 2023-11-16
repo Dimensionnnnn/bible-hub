010100791c22b162c7389be8260067919c5c37f4
dimensionnn <t.golovin55@gmail.com>
2023-11-16 22:18:55 +0600

    feat/init project

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@app': '.src/app',
+          '@pages': '.src/pages',
+          '@widgets': '.src/widgets',
+          '@features': '.src/features',
+          '@entities': '.src/entities',
+          '@shared': '.src/shared',
+        },
+      },
+    ],
+  ],
+};
---
+++ metro.config.js
@@ -0,0 +1,21 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  watchFolders: [
+    path.resolve(__dirname, './src/app'),
+    path.resolve(__dirname, './src/pages'),
+    path.resolve(__dirname, './src/widgets'),
+    path.resolve(__dirname, './src/features'),
+    path.resolve(__dirname, './src/entities'),
+    path.resolve(__dirname, './src/shared'),
+  ],
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
