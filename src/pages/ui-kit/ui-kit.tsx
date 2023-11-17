f65b7efa99aa0e8e03654a8021e3aac39e2f7522
dimensionnn <t.golovin55@gmail.com>
2023-11-18 00:05:30 +0600

    feat/added bottom tabs

---
+++ src/pages/ui-kit/ui-kit.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import {ScrollView, StyleSheet} from 'react-native';
+import {PrimaryButton} from '@shared/ui/buttons/primary-button/primary-button';
+import {ButtonSize as PrimaryButtonSize} from '@shared/ui/buttons/primary-button/primary-button';
+import {SecondaryButton} from '@shared/ui/buttons/secondary-button/secondary-button';
+import {SubscribeButton} from '@shared/ui/buttons/subscribe-button/subscribe-button';
+import {TextButton} from '@shared/ui/buttons/text-button/text-button';
+import {
+  ButtonSize as DeleteButtonSize,
+  DeleteButton,
+} from '@shared/ui/buttons/delete-button/delete-button';
+
+export const UIKit = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <PrimaryButton
+        title="Add"
+        size={PrimaryButtonSize.large}
+        onPress={() => console.log('Pressed')}
+      />
+      <PrimaryButton
+        title="Add"
+        size={PrimaryButtonSize.large}
+        isDisabled
+        onPress={() => console.log('Pressed')}
+      />
+      <PrimaryButton
+        title="Add"
+        size={PrimaryButtonSize.large}
+        isLoading
+        onPress={() => console.log('Pressed')}
+      />
+      <PrimaryButton
+        title="Add"
+        size={PrimaryButtonSize.large}
+        isExit
+        onPress={() => console.log('Pressed')}
+      />
+      <PrimaryButton
+        title="Add"
+        size={PrimaryButtonSize.medium}
+        onPress={() => console.log('Pressed')}
+      />
+      <PrimaryButton
+        title="Add"
+        size={PrimaryButtonSize.small}
+        onPress={() => console.log('Pressed')}
+      />
+      <SecondaryButton title="Add" onPress={() => console.log('Pressed')} />
+      <SecondaryButton
+        title="Add"
+        isDisabled
+        onPress={() => console.log('Pressed')}
+      />
+      <SecondaryButton
+        title="Add"
+        isLoading
+        onPress={() => console.log('Pressed')}
+      />
+      <SubscribeButton
+        title="Following"
+        isFollow
+        onPress={() => console.log('Pressed')}
+      />
+      <SubscribeButton title="Follow" onPress={() => console.log('Pressed')} />
+      <SubscribeButton
+        title="Follow"
+        isLoading
+        onPress={() => console.log('Pressed')}
+      />
+      <SubscribeButton
+        title="Following"
+        isLoading
+        isFollow
+        onPress={() => console.log('Pressed')}
+      />
+      <SubscribeButton
+        title="Following"
+        isDisabled
+        isFollow
+        onPress={() => console.log('Pressed')}
+      />
+      <SubscribeButton
+        title="Follow"
+        isDisabled
+        onPress={() => console.log('Pressed')}
+      />
+      <TextButton title="Log in" onPress={() => console.log('Pressed')} />
+      <TextButton
+        title="Log in"
+        isDisabled
+        onPress={() => console.log('Pressed')}
+      />
+      <DeleteButton onPress={() => console.log('Pressed')} />
+      <DeleteButton
+        size={DeleteButtonSize.small}
+        onPress={() => console.log('Pressed')}
+      />
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    alignItems: 'center',
+    gap: 20,
+  },
+});
---
+++ src/shared/ui/bottom-tabbar/bottom-tabbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {getStyles} from './styles';
+
+export const Tab = createBottomTabNavigator();
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const BottomTabBar: React.FC<Props> = ({children}) => {
+  const styles = getStyles();
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: styles.inconColors.active,
+        tabBarInactiveTintColor: styles.inconColors.inactive,
+        tabBarStyle: [styles.containerColor, styles.container],
+        tabBarLabelStyle: styles.fontLabel,
+      }}>
+      <Tab.Group>{children}</Tab.Group>
+    </Tab.Navigator>
+  );
+};
